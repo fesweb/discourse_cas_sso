2a4104e96ba0b5b674f89ca0ca1ef52a6f29fabe
slattery <michael.slattery@yale.edu>
2016-12-08 12:55:51 -0500

    wip - onmiauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :cas,
-           url: 'http://casino.test'#,
+           url: 'https://secure.its.yale.edu/cas/login'#,
            #on_single_sign_out: Proc.new { |request| LoginController.action(:single_sign_out).call request.env }
 end
